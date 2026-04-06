d438ace5338c35b4b4b36724f2eebdf5e01d2ee3
Nakulvasudev T <nakul.thanigai@gmail.com>
2026-04-06 14:54:21 +0530

    This version displays name from user

--- src/main/java/com/example/HelloApp.java
+++ src/main/java/com/example/HelloApp.java
@@ -1,10 +1,16 @@
 package com.example;
-
 /**
- * Hello world!
+ * HelloApp - UC2
+ * Author: Nakulvasudev T
+ * Version: 2.0
  */
+
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if(args.length > 0) {
+            System.out.println("Hello " + args[0] + "!");
+        } else {
+            System.out.println("Hello World!");
+        }
     }
 }
