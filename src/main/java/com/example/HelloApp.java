107aed0d70bd612712d206f5bc1a6a848c9bba3e
Nakulvasudev T <nakul.thanigai@gmail.com>
2026-04-13 13:11:15 +0530

    UC4 - Multiple Command Line Arguements

--- src/main/java/com/example/HelloApp.java
+++ src/main/java/com/example/HelloApp.java
@@ -7,11 +7,14 @@ package com.example;
 
 public class HelloApp {
     public static void main(String[] args) {
-        String name = "World";
-        if(args.length > 0) {
-            name = args[0];
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i<args.length; i++){
+            sb.append(args[i]);
+            if(i<args.length-1){
+                sb.append(" ");
+            }
         }
-        System.out.println("Hello, " + name + "!");
-        }
+        System.out.println(sb.toString());
     }
+}
 
