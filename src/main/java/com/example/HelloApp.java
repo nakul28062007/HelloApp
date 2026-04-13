2cc170c406e0e202082e666b57b76ff46197afa0
Nakulvasudev T <nakul.thanigai@gmail.com>
2026-04-13 14:34:16 +0530

    UC6: Implemented Substring Method

--- src/main/java/com/example/HelloApp.java
+++ src/main/java/com/example/HelloApp.java
@@ -1,29 +1,22 @@
 package com.example;
 /**
- * HelloApp - UC5: Multiple Command line arguments using StringBuilder with enhanced for loop.
  * Author: Nakulvasudev T
- * Version: 5.0
+ * Version: 6.0
  */
 
 public class HelloApp {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         StringBuilder sb = new StringBuilder();
-        boolean first = true;
-        for (String arg : args) {
-            if(!first){
-                sb.append(" ");
-            }
-            else{
-                sb.append(arg);
-                first = false;
-            }
+        for(String arg: args){
+            sb.append(arg).append(", ");
         }
-        if(sb.length() == 0){
-            System.out.println("Hello, World!");
-        }
-        else{
-            System.out.println("Hello, " + sb.toString() + "!");
+        if(sb.isEmpty()){
+            System.out.println("Hello World!");
+            return;
+        }else {
+            System.out.println("Hello " + sb.substring(0, sb.length() - 2) + "!");
         }
     }
 }
+
 
